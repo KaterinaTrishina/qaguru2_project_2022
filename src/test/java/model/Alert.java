33e85a4f4725e750003912fb37e8d94d425044de
KaterinaTrishina <115570576+KaterinaTrishina@users.noreply.github.com>
2023-02-04 16:33:46 +0200

    New objects for testing Weather site

---
+++ src/test/java/model/Alert.java
@@ -0,0 +1,65 @@
+package model;
+
+import io.cucumber.core.internal.com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class Alert {
+
+    @JsonProperty ("sender_name")
+    private String senderName;
+
+    private String events;
+    private Long starts;
+    private Long end;
+    private String description;
+    private List <String> tags;
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getEvents() {
+        return events;
+    }
+
+    public void setEvents(String events) {
+        this.events = events;
+    }
+
+    public Long getStarts() {
+        return starts;
+    }
+
+    public void setStarts(Long starts) {
+        this.starts = starts;
+    }
+
+    public Long getEnd() {
+        return end;
+    }
+
+    public void setEnd(Long end) {
+        this.end = end;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+}
